e2f5834e3899b98aacfb595926b5eff5fd89ffb3
oguzhanbayram93 <oguzhan_1907_bayram@hotmail.com>
2021-09-17 16:17:02 +0300

    ADDDED CONSOLE.LOG

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ console.log("Hello world!");
 const myName = "Jonas Schmedtmann";
 const h1 = document.querySelector(".heading-primary");
 console.log(myName);
-console.log(h1);
 
+console.log("oguzhan");
 // h1.addEventListener("click", function () {
 //   h1.textContent = myName;
 //   h1.style.backgroundColor = "red";
